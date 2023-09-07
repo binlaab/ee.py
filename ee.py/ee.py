f83b639f68ef29889d301289c9d00ed9b5391b10
BinLAB <root@binlab.tk>
2023-09-07 20:23:16 +0200

    Initial commit

---
+++ ee.py/ee.py
@@ -0,0 +1,8 @@
+import time
+import random
+random.seed()
+
+
+def eep(zzz=random.random() * 10, *args, **kwargs):
+    print("goodnight")
+    time.sleep(zzz)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ee.py',
+    version='1.0',
+    packages=['ee.py'],
+    url='https://github.com/binlaab/ee.py',
+    license='',
+    author='binlaab',
+    author_email='root@binlab.es.eu.org',
+    description='eepy'
+)
